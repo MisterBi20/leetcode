169a13b6d16ccce2b23f00f3213346c0dba4b279
MisterBi <bihailong739@gmail.com>
2025-09-27 15:11:32 +0800

    2025.9.27

---
+++ src/day4/Ex9.java
@@ -0,0 +1,38 @@
+package day4;
+
+/*给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+        回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。*/
+
+import java.util.Scanner;
+
+public class Ex9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        Ex9 ex9 = new Ex9();
+        System.out.println(ex9.isPalindrome(x));
+    }
+
+/*    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        String s = String.valueOf(x);
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }*/
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        int temp = x;
+        int res = 0;
+        while (temp != 0) {
+            res = res * 10 + temp % 10;
+            temp = temp / 10;
+        }
+        return res == x;
+    }
+}
