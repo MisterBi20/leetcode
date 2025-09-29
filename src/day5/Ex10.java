f923d989f4f1473d2032207042e7dd03bccdd161
MisterBi <bihailong739@gmail.com>
2025-09-29 20:45:02 +0800

    2025.9.28

---
+++ src/day5/Ex10.java
@@ -0,0 +1,52 @@
+package day5;
+
+/*给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+
+        '.' 匹配任意单个字符
+        '*' 匹配零个或多个前面的那一个元素
+        所谓匹配，是要涵盖 整个 字符串 s 的，而不是部分字符串。*/
+
+import java.util.Scanner;
+
+public class Ex10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        String p = scanner.nextLine();
+        Ex10 ex10 = new Ex10();
+        System.out.println(ex10.isMatch(s, p));
+    }
+
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+
+        boolean[][] f = new boolean[m + 1][n + 1];
+        f[0][0] = true;
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p.charAt(j - 1) == '*') {
+                    f[i][j] = f[i][j - 2];
+                    if (matches(s, p, i, j - 1)) {
+                        f[i][j] = f[i][j] || f[i - 1][j];
+                    }
+                } else {
+                    if (matches(s, p, i, j)) {
+                        f[i][j] = f[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+        return f[m][n];
+    }
+
+    public boolean matches(String s, String p, int i, int j) {
+        if (i == 0) {
+            return false;
+        }
+        if (p.charAt(j - 1) == '.') {
+            return true;
+        }
+        return s.charAt(i - 1) == p.charAt(j - 1);
+    }
+}
---
+++ src/day5/Ex11.java
@@ -0,0 +1,37 @@
+package day5;
+
+/*给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+        找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+        返回容器可以储存的最大水量。
+        说明：你不能倾斜容器。*/
+
+import java.util.Scanner;
+
+public class Ex11 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int[] height=new int[n];
+        for (int i = 0; i < n; i++) {
+            height[i]=scanner.nextInt();
+        }
+        Ex11 ex11=new Ex11();
+        System.out.println(ex11.maxArea(height));
+    }
+    public int maxArea(int[] height){
+        int n =height.length;
+        int l=0,r=n-1;
+        int ans=0;
+        while (l<r){
+            int area=Math.min(height[l],height[r])*(r-l);
+            ans=Math.max(area,ans);
+            if (height[l]<=height[r]){
+                l++;
+            }else {
+                r--;
+            }
+        }
+        return ans;
+    }
+
+}
