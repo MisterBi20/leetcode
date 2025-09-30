86e67b697c698d008c2007c2c9ecfc1ecb9c606d
MisterBi <bihailong739@gmail.com>
2025-09-30 12:06:05 +0800

    2025.9.30

---
+++ src/day7/Ex15.java
@@ -0,0 +1,48 @@
+package day7;
+
+/*        给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，
+        同时还满足 nums[i] + nums[j] + nums[k] == 0 。请你返回所有和为 0 且不重复的三元组。
+        注意：答案中不可以包含重复的三元组。*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ex15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Ex15 ex15 = new Ex15();
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        System.out.println(ex15.threeSum(nums));
+    }
+
+    //    排序双指针
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ls = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int l = i + 1, r = nums.length - 1, sum = 0 - nums[i];
+                while (l < r) {
+                    if (nums[l] + nums[r] == sum) {
+                        ls.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                        while (l < r && nums[l] == nums[l + 1]) l++;
+                        while (l < r && nums[r] == nums[r - 1]) r--;
+                        l++;
+                        r--;
+                    } else if (nums[l] + nums[r] < sum) {
+                        l++;
+                    } else {
+                        r--;
+                    }
+                }
+            }
+        }
+        return ls;
+    }
+}
