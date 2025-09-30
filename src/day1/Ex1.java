71f817cc28e69e70ecd3afeb5beb9cbc6c87172a
MisterBi <bihailong739@gmail.com>
2025-09-30 12:05:37 +0800

    2025.9.24

--- src/day1/Ex1.java
+++ src/day1/Ex1.java
@@ -1,11 +1,11 @@
 package day1;
 
-import java.util.HashMap;
-import java.util.Scanner;
-
 /*给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
         你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
         你可以按任意顺序返回答案。*/
+
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Ex1 {
     public static void main(String[] args) {
@@ -22,7 +22,7 @@ public class Ex1 {
             System.out.println(indexes[i]);
         }
     }
-
+    /*暴力枚举，时间复杂度O(n^2)*/
     public static int[] twoSum(int[] nums, int target) {
         int[] indexes = new int[2];
         for (int i = 0; i < nums.length; i++) {
@@ -36,7 +36,7 @@ public class Ex1 {
         }
         return indexes;
     }
-
+    /*哈希表，添加所需数和索引到哈希表中，时间复杂度O(n)*/
     public static int[] _twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
--- src/day1/Ex2.java
+++ src/day1/Ex2.java
@@ -12,29 +12,30 @@ public class Ex2 {
 
     }
 
-    public static ListNode addTwoNumber(ListNode l1,ListNode l2){
-        ListNode head =null,tail=null;
-        int carry=0;
-        while (l1!=null||l2!=null){
-            int n1=l1!=null?l1.val:0;
-            int n2=l2!=null?l2.val:0;
-            int sum=n1+n2+carry;
-            if (head==null){
-                head=tail=new ListNode(sum%10);
-            }else {
-                tail.next=new ListNode(sum%10);
-                tail=tail.next;
+    /*模拟加法，从低位到高位，每次取两个链表的节点值相加，时间复杂度O(max(m,n))*/
+    public static ListNode addTwoNumber(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
             }
-            carry=sum/10;
-            if (l1!=null){
-                l1=l1.next;
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
             }
-            if (l2!=null){
-                l2=l2.next;
+            if (l2 != null) {
+                l2 = l2.next;
             }
         }
-        if (carry>0)
-            tail.next=new ListNode(carry);
+        if (carry > 0)
+            tail.next = new ListNode(carry);
         return head;
     }
 }
