2bde183d4cbfdbfe6a00df15111c9ad3f84a158d
MisterBi <bihailong739@gmail.com>
2025-12-26 13:10:28 +0800

    2025.12.26

---
+++ src/day50/Ex617.java
@@ -0,0 +1,33 @@
+package day50;
+
+/*给你两棵二叉树： root1 和 root2 。
+想象一下，当你将其中一棵覆盖到另一棵之上时，两棵树上的一些节点将会重叠（而另一些不会）。
+你需要将这两棵树合并成一棵新二叉树。合并的规则是：如果两个节点重叠，
+那么将这两个节点的值相加作为合并后节点的新值；否则，不为 null 的节点将直接作为新二叉树的节点。
+返回合并后的二叉树。注意: 合并过程必须从两个树的根节点开始。*/
+
+public class Ex617 {
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(1);
+        root1.left = new TreeNode(3);
+        root1.right = new TreeNode(2);
+        root1.left.left = new TreeNode(5);
+        TreeNode root2 = new TreeNode(2);
+        root2.left = new TreeNode(1);
+        root2.right = new TreeNode(3);
+        root2.left.right = new TreeNode(4);
+        root2.right.right = new TreeNode(7);
+        Ex617 ex617 = new Ex617();
+        TreeNode treeNode = ex617.mergeTrees(root1, root2);
+    }
+
+    //    前序遍历，先合并根节点，再合并左子树，最后合并右子树。
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) return root2;
+        if (root2 == null) return root1;
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+        return root1;
+    }
+}
---
+++ src/day50/Ex647.java
@@ -0,0 +1,14 @@
+package day50;
+
+/*给你一个字符串s，请你统计并返回这个字符串中回文子串 的数目。
+回文字符串是正着读和倒过来读一样的字符串。
+子字符串是字符串中的由连续字符组成的一个序列。*/
+
+public class Ex647 {
+    public static void main(String[] args) {
+
+    }
+    public int countSubstrings(String s){
+
+    }
+}
